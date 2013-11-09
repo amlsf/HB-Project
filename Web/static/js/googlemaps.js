bffc38b416a2416006547edd8d9788a8ca2e8333
Hackbright Student <user@mini-005.(none)>
2013-11-08 17:16:33 -0800

    Reset zoom on map

--- Web/static/js/googlemaps.js
+++ Web/static/js/googlemaps.js
@@ -1,7 +1,7 @@
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(37.7577,-122.4376),
-        zoom: 11,
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
