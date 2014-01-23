b9c038ee06a3790b081c470fbd7790a5f610976e
Amy <amyshire@gmail.com>
2014-01-23 14:40:43 -0800

    Debugging

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 # Config file, put all your keys and passwords and whatnot in here
 DB_URI = os.environ.get("DATABASE_URL", "sqlite:///supplycache.db")
 SECRET_KEY = "booyakasha"
+
+DEBUG=True
